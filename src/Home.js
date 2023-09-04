528a762186777b2e24d57e11b456b5af8bd32a13
mikba <bartab@gmail.com>
2023-09-04 09:29:12 -0700

    template

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
-module.exports = `
-<html lang="de-DE">
+module.exports = `<html lang="de-DE">
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
